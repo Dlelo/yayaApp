dbc0a47a2fe6029e1369363b57c1c93154962a2e
donattahakinyi <akinyidonattah@gmail.com>
2025-12-23 22:51:43 +0300

    main

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -4,6 +4,10 @@ import {MatDivider} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
 import {Router} from '@angular/router';
 import {MatButton} from '@angular/material/button';
+import {LoginService} from '../login/login.service';
+import {AccountDetailsService} from '../account-details/account-details.service';
+import {filter, Observable, shareReplay, switchMap} from 'rxjs';
+import {AsyncPipe} from '@angular/common';
 
 @Component({
   selector: 'app-profile',
@@ -12,13 +16,26 @@ import {MatButton} from '@angular/material/button';
     MatIconModule,
     MatCard,
     MatDivider,
-    MatButton
+    MatButton,
+    AsyncPipe
   ],
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
 
   private readonly router:Router = inject(Router);
+  private readonly loginService = inject(LoginService);
+  private readonly accountDetails = inject(AccountDetailsService);
+
+  userId = this.loginService.userId();
+
+  /** Primary stream */
+  userDetails$!: Observable<UserDetails>;
+
+  /** Derived streams */
+  houseHelpDetails$!: Observable<HouseHelp>;
+
+
   houseHelp = {
     name: 'Mary Akinyi',
     role: 'Housekeeper',
@@ -29,6 +46,25 @@ export class ProfileComponent {
     phone: '+254 712 345678',
     email: 'mary@example.com'
   };
+
+  ngOnInit(): void {
+    if (!this.userId) return;
+
+    /** Fetch user ONCE */
+    this.userDetails$ = this.accountDetails
+      .getUserById(this.userId)
+      .pipe(shareReplay(1));
+
+    /** HouseHelp details */
+    this.houseHelpDetails$ = this.userDetails$.pipe(
+      filter(user => user.roles.includes('HOUSEHELP')),
+      filter(user => !!user.houseHelp?.id),
+      switchMap(user =>
+        this.accountDetails.getHouseHelpDetails(user.houseHelp.id)
+      )
+    );
+  }
+
 
   navigate(path: string) {
     this.router.navigate([path]);
