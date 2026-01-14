b3561c611489199d242103e8278079293227c6ed
donattahakinyi <akinyidonattah@gmail.com>
2026-01-14 21:22:14 +0300

    Add filter by type

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -1,39 +1,87 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { MatPaginatorModule, PageEvent } from '@angular/material/paginator';
-import { MatCardModule } from '@angular/material/card';
 import { ActivatedRoute, Router } from '@angular/router';
-import { MatButton } from '@angular/material/button';
 import { HousehelpService } from '../dashboard/house-helps/house-helps.service';
-import { AsyncPipe, NgFor, NgIf } from '@angular/common';
-import { Observable, switchMap } from 'rxjs';
+import { Observable } from 'rxjs';
+import {MatPaginator, PageEvent} from '@angular/material/paginator';
+import {FormsModule} from '@angular/forms';
+import {MatCard, MatCardActions, MatCardContent} from '@angular/material/card';
+import {AsyncPipe} from '@angular/common';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   standalone: true,
   selector: 'app-listings',
-  imports: [MatPaginatorModule, MatCardModule, MatButton, AsyncPipe],
   templateUrl: './listing.component.html',
   styleUrls: ['./listing.component.scss'],
   providers: [HousehelpService],
+  imports: [
+    FormsModule,
+    MatCard,
+    AsyncPipe,
+    MatCardContent,
+    MatCardActions,
+    MatButton,
+    MatPaginator
+  ]
 })
 export class ListingsComponent implements OnInit {
-  private readonly router: Router = inject(Router);
-  private readonly househelpService:HousehelpService = inject(HousehelpService);
-  private readonly activatedRoute: ActivatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  private route = inject(ActivatedRoute);
+  private househelpService = inject(HousehelpService);
 
-  houseHelps$: Observable<{ data: any[]; length: number; pages: any }> | undefined;
+  houseHelps$!: Observable<any>;
+
+  page:number = 0;
+  size:number = 20;
+  type:string = 'ALL';
+
+  filters: any = {
+    active: true,
+    houseHelpType: null,
+    experience: null,
+    minExpectedSalary: null,
+    maxExpectedSalary: null,
+    location: null,
+    languages: null,
+  };
 
   ngOnInit() {
-    this.houseHelps$ = this.activatedRoute.paramMap.pipe(
-      switchMap(params => {
-        const type = params.get('type') || 'all';
-        return this.househelpService.getAll(type.toUpperCase());
-      })
+    this.route.paramMap.subscribe(params => {
+      this.type = (params.get('type') || 'all').toUpperCase();
+      this.filters.houseHelpType = this.type === 'ALL' ? null : this.type;
+      this.load(this.type);
+    });
+  }
+
+  load(type:string) {
+    this.houseHelps$ = this.househelpService.getAll(
+      type,
+      this.page,
+      this.size,
+      this.filters
     );
+  }
+
+  applyFilters() {
+    this.page = 0;
+    this.load(this.type);
+  }
+
+  clearFilters() {
+    this.filters = {
+      active: true,
+      houseHelpType: this.filters.houseHelpType,
+    };
+    this.load(this.type);
+  }
+
+  onPageChange(event: PageEvent) {
+    this.page = event.pageIndex;
+    this.size = event.pageSize;
+    this.load(this.type);
   }
 
   seeDetails(id: number) {
     this.router.navigate(['/profile', id]);
   }
-
-  onPageChange(event: PageEvent) { console.log('Load page', event.pageIndex); }
 }
