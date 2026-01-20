cf21f0aa618dd94d1ff43b5f4c1aa759bd4a33ba
donattahakinyi <akinyidonattah@gmail.com>
2026-01-20 09:42:28 +0300

    update

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,6 +3,5 @@ export const environment = {
   apiUrl: 'https://api.yayaconnectapp.com/api',
   // apiUrl: 'http://localhost:8080/api',
   paybillNumber: 522522,
-  accountNumber: 240925,
-  mpesaApiUrl:'http://localhost:8080/api'
+  accountNumber: 240925
 };
