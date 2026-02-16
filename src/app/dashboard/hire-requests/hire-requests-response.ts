58727f7f89a3c93e87f8fe544bad05105f3e1237
new_t <new_t@example.com>
2026-02-16 23:12:53 +0300

    Fetch and display hire requests

--- src/app/dashboard/hire-requests/hire-requests-response.ts
+++ src/app/dashboard/hire-requests/hire-requests-response.ts
@@ -1,16 +1,16 @@
-interface PageResponse<T> {
-  content: T[];
-  totalElements: number;
-  totalPages: number;
-  number: number;
-  size: number;
-  first: boolean;
-  last: boolean;
-}
-
 interface HireRequest {
   id: number;
-  owner: User;
-  househelp: User;
-  date: Date;
+  createdAt: Date;
+  paid: boolean;
+  homeOwner: {
+    id: number;
+    name: string;
+  };
+  houseHelp: {
+    id: number;
+    name: string;
+  };
+  status: 'PENDING' | 'ACCEPTED' | 'REJECTED';
+  startDate: Date;
+  message: string;
 }
